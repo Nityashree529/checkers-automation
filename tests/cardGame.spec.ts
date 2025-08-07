94073d414736f3862906056497b330dcf07a095d
Nityashree <nithyamahendra25@gmail.com>
2025-08-07 17:32:11 -0600

    card game api test script add execution comments

--- tests/cardGame.spec.ts
+++ tests/cardGame.spec.ts
@@ -3,26 +3,33 @@ import { request } from 'http'
 
 test('card game' , async({request})=> {
 
+    // Check for site is up and running
     const url = 'https://deckofcardsapi.com/api/deck/new/';
     const deckRes = await request.get(url);
    expect(deckRes.ok()).toBeTruthy();
 
+   // Obtain new deck id
      const deckData = await deckRes.json();
      const deckId = deckData.deck_id;
      console.log('Deck ID:', deckId);
 
+     //Shuffle it
      const shuffleRes = await request.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
     expect(shuffleRes.ok()).toBeTruthy();
      const shuffleResponse = await shuffleRes.json();
      expect(shuffleResponse.deck_id).toBe(deckId);
      
-
+//Deal three cards to each of two players
      const drawRes = await request.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=6`);
      const drawData = await drawRes.json();
      const cards = drawData.cards;
+      expect(drawData.deck_id).toBe(deckId);
+
+
       const player1 = cards.slice(0, 3);
       const player2 = cards.slice(3, 6);
 
+ // function to check the sum of cards
       function getScore(cardScore) {
   let sum = 0;
 
@@ -44,6 +51,8 @@ const p2Score = getScore(player2);
 
 console.log("Player 1 Totalscore is: " + p1Score);
 console.log("Player 2 Totalscore is : " + p2Score);
+
+// To check whether either has blackjack
 
 if (p1Score === 21 && p2Score === 21) {
   console.log("Both players have Blackjack!");
